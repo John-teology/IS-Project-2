5376d63f2fbafea4c147b876ae291a37b04f0c45
John-Teology <teologo.wilhelm@gmail.com>
2022-04-26 13:28:41 +0800

    uid

--- portfolio/views.py
+++ portfolio/views.py
@@ -6,10 +6,15 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse 
 from django.contrib.auth.decorators import login_required
+from allauth.socialaccount.models import SocialAccount
 
 from .models import *
 
 # Create your views here.
+@login_required
 def index(request):
-    return render(request, "portfolio/index.html")
+    data = SocialAccount.objects.get(user=request.user).extra_data
+    return render(request, "portfolio/index.html",{
+        'data' : data['login']
+    })
 
