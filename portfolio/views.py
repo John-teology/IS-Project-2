39a0cf417fbac48f66e66f6b9b0a94aaecd7853a
John-Teology <teologo.wilhelm@gmail.com>
2022-05-26 22:34:31 +0800

    save

--- portfolio/views.py
+++ portfolio/views.py
@@ -23,15 +23,23 @@ def leader(request):
     return render(request, "portfolio/leaderboards.html")
 
 
-
+# https://avatars.githubusercontent.com/John-teology github profile image hehe
 def read(request):
     x = requests.get('https://api.github.com/users/john-teology/repos')
     data = x.json()
     lang = []
     for i in range(len(data)):
-        lang.append(data[i]['language'])
+        lang.append(data[i]['name'])
+    sum = []
+    for a in lang:
+        y = requests.get(f'https://api.github.com/repos/John-teology/{a}/languages')
+        data1 = y.json()
+        try:
+            sum.append(data1['JavaScript'])  
+        except KeyError:
+            sum.append(0)  
     return render(request, "portfolio/read.html", {
-        'data' : lang
+        'data' : sum
     })
 
     
