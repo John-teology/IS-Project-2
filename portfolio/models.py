49c225815b7e239408983ed54be5946324261e43
John-Teology <teologo.wilhelm@gmail.com>
2022-05-31 14:48:39 +0800

    db

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,23 +1,135 @@
-from statistics import mode
+from turtle import ondrag
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.deletion import CASCADE
 # Create your models here.
 
 class User(AbstractUser):
     pass
 
-
-class Profile(models.Model):
-    pass
-
-
 class Course(models.Model):
-    pass
+    course = models.CharField(max_length=50)
 
 
 class YearLevel(models.Model):
-    pass
+    yearLevel = models.CharField(max_length=50)
+
 
 
 class LeaderBoards(models.Model):
     pass
+
+
+class Profile(models.Model):
+    userID = models.ForeignKey(User, on_delete=CASCADE, related_name= 'userProfile' )
+    courseID = models.ForeignKey(Course, on_delete=CASCADE, related_name='userCourse' )
+    yearId = models.ForeignKey(YearLevel, on_delete=CASCADE, related_name= 'userYearLevel')
+
+
+
+
+
+
+
+"""
+1C Enterprise
+ASP.NET
+ActionScript
+Apex
+Assembly
+Ballerina
+Batchfile
+Blazor
+C
+C#
+C++
+COBOL
+CSS
+Clojure
+CoffeeScript
+Crystal
+Cucumber
+Dart
+Delphi
+Dockerfile
+EJS
+EPP
+ERB
+Ebuild
+Elixir
+Elm
+Erlang
+F#
+Fortran
+Go
+Groovy
+HCL
+HTML
+HTML+Razor
+Haskell
+Haxe
+HiveQL
+JSON
+Java
+JavaScript
+Julia
+Jupyter Notebook
+Kivy
+Kotlin
+LabVIEW
+Less
+Lex
+Limbo
+Liquid
+Lua
+M
+MATLAB
+Makefile
+Mathematica
+Matlab
+Mercury
+Nginx
+Nix
+Objective-C
+Objective-C++
+OpenEdge ABL
+PHP
+PLSQL
+PLpgSQL
+Perl
+Perl 6
+PowerBuilder
+Powershell
+Prolog
+Protocol Buffer
+Puppet
+Python
+QML
+R
+Raku
+Robot
+Ruby
+Rust
+SASS
+SCSS
+SQL
+SQLPL
+Scala
+Shell
+Smalltalk
+Solidity
+Stata
+Stylus
+Svelte
+Swift
+TSQL
+TypeScript
+Vue
+XML
+Xtend
+Xtext
+YAML
+Yacc
+Zig
+
+"""
--- portfolio/views.py
+++ portfolio/views.py
@@ -10,6 +10,7 @@ from django.contrib.auth.decorators import login_required
 from allauth.socialaccount.models import SocialAccount
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 
 from .models import *
@@ -58,56 +59,70 @@ def read(request):
 
 
 
-# import requests
-# from bs4 import BeautifulSoup
 
-# import pandas as pd
 
-# # name = 'jaeam17'
-# name = 'John-teology'
+def get_score(name):
+    # name = 'jaeam17'
+    name = 'John-teology'
 
-# x = requests.get(f'https://api.github.com/users/{name}/repos')
-# data = x.json()
-# prog = []
-# langu = []
-# lang_dict = {}
-# for i in range(len(data)):
-#     langu.append(data[i]['name'])
+    x = requests.get(f'https://api.github.com/users/{name}/repos')
+    data = x.json()
+    prog = []
+    langu = []
+    lang_dict = {}
+    for i in range(len(data)):
+        langu.append(data[i]['name'])
 
-# for la in langu:
-#     html_text = requests.get(f'https://github.com/{name}/{la}').content
-#     soup = BeautifulSoup(html_text, 'lxml')
-#     langs = soup.find_all('a',class_ = 'd-inline-flex flex-items-center flex-nowrap Link--secondary no-underline text-small mr-3')
-#     for lang in langs:
-#         d = lang.find_all('span')
-#         for a in d:
-#             prog.append(a.text.replace("%",""))
+    for la in langu:
+        html_text = requests.get(f'https://github.com/{name}/{la}').content
+        soup = BeautifulSoup(html_text, 'lxml')
+        langs = soup.find_all('a',class_ = 'd-inline-flex flex-items-center flex-nowrap Link--secondary no-underline text-small mr-3')
+        for lang in langs:
+            d = lang.find_all('span')
+            for a in d:
+                prog.append(a.text.replace("%",""))
+        
+        lang_dict[la] = {prog[i]: prog[i + 1] for i in range(0, len(prog), 2)}
+        prog[:] = []
+
+
+    df = pd.DataFrame(lang_dict)
+    df.index.name = "languages"
+    df = df.astype(float)
+    df = df.reset_index(level=0)
+    df = df.fillna(0)
+    df['lang_score'] = df.sum(axis=1, numeric_only=True)
+
+
+    into_db = { }
+
+    dat = df['languages']
+
+    for d in dat:
+        val = df[df['languages'] == d ]['lang_score']
+        into_db[d] = float(val.values)
+        
+    print(into_db)
+    print(df['lang_score'].sum())
+
+
+def test(request):
+    input = { 'Python': 100, 'Java' :20, 'CSS':20,"hahah":43}
+    saving(input)
+    return HttpResponse('Success')
     
-#     lang_dict[la] = {prog[i]: prog[i + 1] for i in range(0, len(prog), 2)}
-#     prog[:] = []
-
-
-# df = pd.DataFrame(lang_dict)
-# df.index.name = "languages"
-# df = df.astype(float)
-# df = df.reset_index(level=0)
-# df = df.fillna(0)
-# df['lang_score'] = df.sum(axis=1, numeric_only=True)
-
-
-# into_db = { }
-
-# dat = df['languages']
-
-# for d in dat:
-#     val = df[df['languages'] == d ]['lang_score']
-#     into_db[d] = float(val.values)
     
-# print(into_db)
-# print(df['lang_score'].sum())
-
-
-
+def saving(input):
+    import re
+    try:
+        d = Leadtest(name = 'cd', **input)
+        d.save()
+        
+    except TypeError as e:
+        s = str(e)
+        result = re.search("'(.*)'", s)
+        del input[result.group(1)]
+        saving(input)
 
 
 
