8d053a68de897ad21c2af050cdbef216a89be1f1
John-Teology <teologo.wilhelm@gmail.com>
2022-06-13 20:53:52 +0800

    update settings

--- ISP/settings.py
+++ ISP/settings.py
@@ -233,8 +233,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Configure Django App for Heroku.
-django_heroku.settings(locals())
 
 CRISPY_TENPLATE_PACK = "baotstrap4"
 
 
+django_heroku.settings(locals())
