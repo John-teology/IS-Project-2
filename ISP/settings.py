1a42be6de008e31357757868624ac74576db051d
John-Teology <teologo.wilhelm@gmail.com>
2022-06-13 21:02:42 +0800

    decouple

--- ISP/settings.py
+++ ISP/settings.py
@@ -11,7 +11,7 @@ from pathlib import Path
 import os
 import django_heroku
 import dj_database_url
-from decouple import config,Csv
+from decouple import config
 
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
