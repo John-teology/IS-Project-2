bd41a9da01274f8f1ab6d1c98cec015b34771359
John-teology <teologowilhlem@gmail.com>
2022-06-13 16:37:11 +0800

    deploy10

--- ISP/settings.py
+++ ISP/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
