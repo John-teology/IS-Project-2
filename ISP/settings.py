311ee619b5e11560d8b8c6d0705e80b90545fc58
John-Teology <teologo.wilhelm@gmail.com>
2022-06-14 11:55:56 +0800

    db settings

--- ISP/settings.py
+++ ISP/settings.py
@@ -9,6 +9,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,23 +92,17 @@ WSGI_APPLICATION = 'ISP.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': ['d3kll2sir0i7ld'],
-        'USER': ['snohboewpobphi'],
-        'PASSWORD': ['cd48528f597b5c0ecd0229f50291ae9a8ba2b455474b39feb475fb2e207f7f86'],
-        'HOST': ['ec2-52-72-99-110.compute-1.amazonaws.com'],
-        'PORT':  ['5432']
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
